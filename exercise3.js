fe83bc19fb5f76bd79096ef52f5ad075c8331bfa
memorvin <alvina.tjia@yahoo.com>
2019-09-23 23:54:14 +0700

    exercise3

--- exercise3.js
+++ exercise3.js
@@ -1,21 +1,21 @@
-var nama = prompt("Silakan isi nama");
+var nama = "";
+var peran = "";
 
-if (nama === null) {
-    console.log("Nama harus diisi!");
-} else {
-    console.log("Selamat datang di Dunia Proxytia," + nama);
-}
-
-var peran = prompt("Silakan pilih peran sebagai Ksatria, Tabib, atau Penyihir");
-
-if (nama != null && peran === null) {
-    console.log("Halo" + nama + ", Pilih peranmu untuk memulai game!");
-} else {
-    if (peran = 'ksatria'){
-        console.log("Halo Ksatria" + nama + ", kamu dapat menyerang dengan senjatamu!")
-    } else if (peran = 'tabib'){
-        console.log("Halo Tabib" + nama + ", kamu akan membantu temanmu yang terluka.")
-    } else {
-        console.log("Halo Penyihir" + nama + ", ciptakan keajaiban yang membantu kemenanganmu!")
+if (nama) {
+    if (peran) {
+        console.log("Selamat datang di Dunia Proxytia, " + nama);
+        if (peran = "Ksatria") {
+            console.log("Halo Ksatria " + nama + ", kamu dapat menyerang dengan senjatamu!");
+        } else if (peran = "Tabib") {
+            console.log("Halo Tabib " + nama + ", kamu akan membantu temanmu yang terluka.");
+        } else {
+            console.log("Halo Penyihir " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
+        }
     }
+    else {
+        console.log("Halo " + nama + ", Pilih peranmu untuk memulai game!");
+    }
+}
+else {
+    console.log("Nama harus diisi!");
 }
