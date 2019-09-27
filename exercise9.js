5444289fa16ea20c8a7d415e965c81dc50dbdc96
memorvin <alvina.tjia@yahoo.com>
2019-09-27 22:48:54 +0700

    exercise9

--- exercise9.js
+++ exercise9.js
@@ -23,6 +23,5 @@ function processSentence(name, age, address, hobby) {
     return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + "!";
 }
 
-
 var fullSentence = processSentence("Agus", 30, "Jln. Malioboro, Yogjakarta", "gaming");
 console.log(fullSentence);
