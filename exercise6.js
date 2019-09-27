33c94e62614b135a3721092cff80b0f7168a307e
memorvin <alvina.tjia@yahoo.com>
2019-09-27 21:40:22 +0700

    exercise6

---
+++ exercise6.js
@@ -0,0 +1,66 @@
+//1. Melakukan Looping Menggunakan While
+
+    //Looping pertama
+var i = 2;
+while (i <= 20) {
+    console.log(i + " - I love coding");
+    i += 2;
+}
+
+    //Looping kedua
+var i = 20;
+while (i >= 2) {
+    console.log(i + " I will become fullstack developer");
+    i -= 2;
+}
+
+
+//2. Melakukan Looping Menggunakan For
+
+    //Looping pertama
+for (var i = 1; i <= 20; i++) {
+    console.log(i + " - I love coding");
+}
+
+    //Looping kedua
+
+for (var i = 20; i >= 1; i--) {
+    console.log(i + " I will become fullstack developer");
+}
+
+
+//3. Angka Ganjil dan Genap
+
+    //Ganjil genap
+
+for (var counter = 1; counter <= 100; counter++) {
+    if (counter % 2 === 0) {
+      console.log("GENAP");
+    } else {
+      console.log("GANJIL");
+    }
+}
+
+    //Kelipatan tiga
+
+for (var counter = 1; counter <= 100; counter+=2) {
+    if (counter % 3 === 0) {
+      console.log(counter + " kelipatan 3");
+    }
+}
+
+    //Kelipatan enam
+
+for (var counter = 1; counter <= 100; counter+=5) {
+    if (counter % 6 === 0) {
+      console.log(counter + " kelipatan 6");
+    }
+}
+
+    //Kelipatan 10
+
+for (var counter = 1; counter <= 100; counter+=9) {
+    if (counter % 10 === 0) {
+      console.log(counter + " kelipatan 10");
+    }
+}
