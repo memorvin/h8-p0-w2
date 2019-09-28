1f33a5c210548fcd20a56516ed932c89cc8a113a
memorvin <alvina.tjia@yahoo.com>
2019-09-28 12:10:04 +0700

    exercise12

---
+++ exercise12.js
@@ -0,0 +1,14 @@
+//Konversi Menit
+
+function konversiMenit(angka) {
+    var jam = Math.floor(angka/60);
+    var menit = String((angka%60)/10);
+    var result = jam + ':' + menit.slice(0,1) + menit.slice(-1);
+    return result;
+  }
+  
+console.log(konversiMenit(63));
+console.log(konversiMenit(124));
+console.log(konversiMenit(53));
+console.log(konversiMenit(88));
+console.log(konversiMenit(120));
