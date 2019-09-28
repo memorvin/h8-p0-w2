099c7a0d47d438ab9ad5ac0a9ee662bfee456d3a
memorvin <alvina.tjia@yahoo.com>
2019-09-28 10:55:12 +0700

    exercise11

---
+++ exercise11.js
@@ -0,0 +1,15 @@
+//Balik Kata
+
+function balikKata(kata) {
+    var result = '';
+    for (var i = kata.length-1; i >= 0; i--) {
+      result += kata[i];
+    }
+    return result;
+  }
+  
+  console.log(balikKata('Hello World and Coders'));
+  console.log(balikKata('John Doe'));
+  console.log(balikKata('I am a bookworm'));
+  console.log(balikKata('Coding is my hobby'));
+  console.log(balikKata('Super'));
