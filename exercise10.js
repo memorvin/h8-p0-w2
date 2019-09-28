531cd302e2e3f09aa5fc75e05a497c41ab54342c
memorvin <alvina.tjia@yahoo.com>
2019-09-28 08:15:39 +0700

    exercise10

---
+++ exercise10.js
@@ -0,0 +1,19 @@
+//Bandingkan Angka
+
+function bandingkanAngka(angka1, angka2) {
+  if (angka1 < angka2) {
+    return "true";
+  } else if (angka1 > angka2) {
+    return "false";
+  } else if (angka1 === angka2) {
+    return -1;
+  } else {
+    return "invalid";
+  }
+}
+
+console.log(bandingkanAngka(5, 8));
+console.log(bandingkanAngka(5, 3));
+console.log(bandingkanAngka(4, 4));
+console.log(bandingkanAngka(3, 3));
+console.log(bandingkanAngka(17, 2));
