a2a1fa8213e7c7d11dce56121dffe2f774df5bf3
memorvin <alvina.tjia@yahoo.com>
2019-10-01 15:04:46 +0700

    edit exercise2

--- exercise3.js
+++ exercise3.js
@@ -4,9 +4,9 @@ var peran = "";
 if (nama) {
     if (peran) {
         console.log("Selamat datang di Dunia Proxytia, " + nama);
-        if (peran = "Ksatria") {
+        if (peran === "Ksatria") {
             console.log("Halo Ksatria " + nama + ", kamu dapat menyerang dengan senjatamu!");
-        } else if (peran = "Tabib") {
+        } else if (peran === "Tabib") {
             console.log("Halo Tabib " + nama + ", kamu akan membantu temanmu yang terluka.");
         } else {
             console.log("Halo Penyihir " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!");
