0bc1c6500668d9be78036b410803f3e1422426c1
memorvin <alvina.tjia@yahoo.com>
2019-10-01 15:09:45 +0700

    edit exercise6

--- exercise6.js
+++ exercise6.js
@@ -1,13 +1,13 @@
 //1. Melakukan Looping Menggunakan While
 
-    //Looping pertama
+console.log("LOOPING PERTAMA");
 var i = 2;
 while (i <= 20) {
     console.log(i + " - I love coding");
     i += 2;
 }
 
-    //Looping kedua
+console.log("LOOPING KEDUA");
 var i = 20;
 while (i >= 2) {
     console.log(i + " I will become fullstack developer");
@@ -17,13 +17,12 @@ while (i >= 2) {
 
 //2. Melakukan Looping Menggunakan For
 
-    //Looping pertama
+console.log("LOOPING PERTAMA");
 for (var i = 1; i <= 20; i++) {
     console.log(i + " - I love coding");
 }
 
-    //Looping kedua
-
+console.log("LOOPING KEDUA");
 for (var i = 20; i >= 1; i--) {
     console.log(i + " I will become fullstack developer");
 }
